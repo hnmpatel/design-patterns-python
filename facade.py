ffd2c2ea7b8bfff8976138f08cdb2dfc4c00ec4b
Hardik Patel <hpatel@aaravtech.com>
2019-03-18 21:22:10 +0530

    Facade pattern example is implemented.

---
+++ facade.py
@@ -0,0 +1,30 @@
+class Cook(object):
+    def prepareDish(self):
+        self.cutter = Cutter()
+        self.cutter.cutVegetables()
+
+        self.boiler = Boiler()
+        self.boiler.boilVegetables()
+
+        self.frier = Frier()
+        self.frier.fry()
+
+
+class Cutter(object):
+    def cutVegetables(self):
+        print("All vegetables are cut")
+
+
+class Boiler(object):
+    def boilVegetables(self):
+        print("All vegetables are boiled")
+
+
+class Frier(object):
+    def fry(self):
+        print("All vegetables is mixed and fried.")
+
+
+if __name__ == "__main__":
+    cook = Cook()
+    rest.prepareDish()
